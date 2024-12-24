a0f0abd731f32726cdcad1243e6119dd0aeeb9e0
Provakar Dhar Surzo <surzodhar45@gmail.com>
2024-12-25 01:21:52 +0600

    Profile_Rec
    Key Elements Added:
    Profile Image: An ImageView with a placeholder image (you'll need to add a drawable resource).
    User Name: A TextView for the user's name.
    User Description: A TextView for a short bio or description.

--- Dev Files/Version 2.0.1/HomeDesk/app/src/main/java/com/example/recyclerview/RecentFragment.kt
+++ Dev Files/Version 2.0.1/HomeDesk/app/src/main/java/com/example/recyclerview/RecentFragment.kt
@@ -1,4 +1,28 @@
 package com.example.recyclerview
 
-class RecentFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.recyclerview.databinding.FragmentRecentBinding
+
+class RecentFragment : Fragment() {
+
+    private var _binding: FragmentRecentBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRecentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
